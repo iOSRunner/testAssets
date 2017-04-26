8600cea38786b9a499f239e5302ed84fbf3c2666
文楷 <wanlei.gwl@alibaba-inc.com>
2017-04-26 11:41:07 +0800

    init project

---
+++ testAssets/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testAssets
+//
+//  Created by Wanlei on 2017/4/26.
+//  Copyright © 2017年 organizer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ testAssets/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testAssets
+//
+//  Created by Wanlei on 2017/4/26.
+//  Copyright © 2017年 organizer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
